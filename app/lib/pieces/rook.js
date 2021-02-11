6b4bac07ead6abceb677e2d604098490a15e13a6
Steve Szczecina <steveszc@gmail.com>
2021-02-10 17:41:39 -0700

    add rook move logic

--- app/lib/pieces/rook.js
+++ app/lib/pieces/rook.js
@@ -4,6 +4,33 @@ export default class Rook extends Piece {
   type = "rook";
 
   get availablePositions() {
-    return [];
+    return [
+      ...this.getMoves((position) => this.forward(position)),
+      ...this.getMoves((position) => this.right(position)),
+      ...this.getMoves((position) => this.back(position)),
+      ...this.getMoves((position) => this.left(position)),
+    ].filter(Boolean);
+  }
+
+  getMoves(move) {
+    let positions = [];
+    let position = this.position;
+
+    while (true) {
+      let newPosition = move(position);
+      let isOnBoard = this.board.isPositionOnBoard(newPosition);
+      let pieceAtPosition = this.board.pieceAtPosition(newPosition);
+
+      if (
+        isOnBoard &&
+        (!pieceAtPosition || pieceAtPosition.color !== this.color)
+      ) {
+        positions.push(newPosition);
+        position = newPosition;
+      }
+
+      if (!isOnBoard || pieceAtPosition) break;
+    }
+    return positions;
   }
 }
