b5f94d5b43553d72457821f9c474f81197f74acc
Steve Szczecina <steveszc@gmail.com>
2021-02-10 17:38:53 -0700

    Add bishop move logic

--- app/lib/pieces/bishop.js
+++ app/lib/pieces/bishop.js
@@ -4,6 +4,33 @@ export default class Bishop extends Piece {
   type = "bishop";
 
   get availablePositions() {
-    return [];
+    return [
+      ...this.getMoves((position) => this.forward(this.left(position))),
+      ...this.getMoves((position) => this.forward(this.right(position))),
+      ...this.getMoves((position) => this.back(this.left(position))),
+      ...this.getMoves((position) => this.back(this.right(position))),
+    ].filter(Boolean);
+  }
+
+  getMoves(move) {
+    let positions = [];
+    let position = this.position;
+
+    while (true) {
+      let newPosition = move(position);
+      let isOnBoard = this.board.isPositionOnBoard(newPosition);
+      let pieceAtPosition = this.board.pieceAtPosition(newPosition);
+
+      if (
+        isOnBoard &&
+        (!pieceAtPosition || pieceAtPosition.color !== this.color)
+      ) {
+        positions.push(newPosition);
+        position = newPosition;
+      }
+
+      if (!isOnBoard || pieceAtPosition) break;
+    }
+    return positions;
   }
 }
