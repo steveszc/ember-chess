c1f43cf66bad41fcbdbc51fcfc0f0c9ee20c0e23
Steve Szczecina <steveszc@gmail.com>
2022-07-22 22:24:01 -0600

    TS components

--- app/components/game.ts
+++ app/components/game.ts
@@ -1,32 +1,36 @@
 import Component from '@glimmer/component';
+import Board from 'ember-chess/lib/board';
+import Fen from 'ember-chess/lib/fen';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { task, timeout } from 'ember-concurrency';
-import Board from 'ember-chess/lib/board';
-import Fen from 'ember-chess/lib/fen';
 
-export default class GameComponent extends Component {
-  @tracked board = new Board();
+import { Color, GameMode, Turn } from 'ember-chess/lib/types';
+
+interface Args {}
+
+export default class GameComponent extends Component<Args> {
+  @tracked board = new Board(new Fen());
 
   @tracked turn = 1;
-  @tracked turnColor = 'white';
+  @tracked turnColor: Color = 'white';
   @tracked isShowingGuide = false;
   @tracked isShowingKey = false;
   @tracked isShowingLog = false;
-  @tracked gameMode = 'tabletop'; // tabletop | pass-and-play
-  @tracked previousTurnInfo = null;
-  @tracked fen = null;
+  @tracked gameMode: GameMode = 'tabletop';
+  @tracked previousTurnInfo?: Turn;
+  @tracked fen?: string;
 
   @action resetGame() {
     let fen = new Fen(this.fen);
     this.board = new Board(fen);
     this.turn = fen.isValid ? fen.fullTurnCount : 1;
     this.turnColor = fen.isValid ? fen.turnColor : 'white';
-    this.fen = null;
+    this.fen = undefined;
   }
 
   @task
-  *incrementTurn(turn) {
+  *incrementTurn(turn: Turn) {
     yield timeout(500);
 
     if (this.turnColor === 'black') {
@@ -50,11 +54,14 @@ export default class GameComponent extends Component {
     this.isShowingLog = !this.isShowingLog;
   }
 
-  @action setGameMode(mode) {
+  @action setGameMode(mode: GameMode) {
     this.gameMode = mode;
   }
 
-  @action updateFen(event) {
-    this.fen = event.target.value;
+  @action updateFen(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const { value } = target;
+
+    this.fen = value;
   }
 }
