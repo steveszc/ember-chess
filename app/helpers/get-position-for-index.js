6339be8f96b4fb099ac34317853a3726b982ebaa
Steve Szczecina <steveszc@gmail.com>
2022-07-22 22:22:21 -0600

    TS helpers

--- app/helpers/get-position-for-index.js
+++
@@ -1,7 +0,0 @@
-import { helper } from '@ember/component/helper';
-const indexToLetter = (number) => String.fromCharCode(97 + number);
-const indexToNumber = (number) => `${8 - number}`;
-
-export default helper(function getPositionForIndex([row, col] /*, hash*/) {
-  return `${indexToLetter(col)}${indexToNumber(row)}`;
-});
