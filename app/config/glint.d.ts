6fe4b6e4b78ade7283a544882205c552aa69fc3d
Steve Szczecina <steveszc@gmail.com>
2022-07-23 00:22:09 -0600

    Glint install

---
+++ app/config/glint.d.ts
@@ -0,0 +1,34 @@
+import '@glint/environment-ember-loose';
+
+import DidInsertModifier from '@gavant/glint-template-types/types/ember-render-modifiers/did-insert';
+import AndHelper from '@gavant/glint-template-types/types/ember-truth-helpers/and';
+import EqHelper from '@gavant/glint-template-types/types/ember-truth-helpers/eq';
+import NotHelper from '@gavant/glint-template-types/types/ember-truth-helpers/not';
+import PerformHelper from '@gavant/glint-template-types/types/ember-concurrency/perform';
+
+import Helper from '@ember/component/helper';
+
+interface MathHelperSignature {
+    Args: { Positional: [number, number] };
+    Return: number;
+}
+
+interface SetHelperSignature {
+  Args: { Positional: [object, string]};
+  Return: () => void;
+}
+
+declare class MathHelper extends Helper<MathHelperSignature> {}
+declare class SetHelper extends Helper<SetHelperSignature> {}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry {
+    and: typeof AndHelper;
+    eq: typeof EqHelper;
+    not: typeof NotHelper;
+    perform: typeof PerformHelper;
+    set: typeof SetHelper;
+    sub: typeof MathHelper;
+    'did-insert': typeof DidInsertModifier;
+  }
+}
